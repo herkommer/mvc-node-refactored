aa40c0cbc5722965ea67857f910f785f0d259290
herkommer <m@herkommer.co.uk>
2018-11-14 13:31:17 +0100

    cleaned app.js

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-var app = express();
-// app.use(express.static(__dirname + 'www'));
-
-
+var mongoose = require('mongoose');
 var product = require('./routes/product.route');
 var home = require('./routes/home.route');
 
-var mongoose = require('mongoose');
 var db_url = 'mongodb://dbuser:passw0rd@ds155663.mlab.com:55663/te4-papp';
 mongoose.connect( db_url, {useNewUrlParser: true}); //depcreated error removed
 mongoose.Promise=global.Promise; //vi ska prata promises senare
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 app.use('/', home)
 app.use('/products', product);
 
